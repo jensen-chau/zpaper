8ba6e37987421a22d48c81cac8f17340d2966762
zjx <824392528@qq.com>
2026-03-30 19:40:46 +0800

    fix: use calloc to initialize wayland_state to prevent segfault

--- src/wayland_state.c
+++ src/wayland_state.c
@@ -8,7 +8,7 @@
 
 struct wayland_state *wayland_state_new() {
   struct wayland_state *state =
-      (struct wayland_state *)malloc(sizeof(struct wayland_state));
+      (struct wayland_state *)calloc(1, sizeof(struct wayland_state));
   state->display = wl_display_connect(NULL);
   struct wl_registry *registry = wl_display_get_registry(state->display);
   wl_registry_add_listener(registry, &wl_registry_listener, state);
@@ -28,7 +28,8 @@ int create_layer_surface(struct wayland_state *state) {
     struct output_info *info = state->output_infos[i];
     info->surface = wl_compositor_create_surface(state->compositor);
 
-    info->viewport = wp_viewporter_get_viewport(state->viewporter, info->surface);
+    info->viewport =
+        wp_viewporter_get_viewport(state->viewporter, info->surface);
 
     info->layer_surface = zwlr_layer_shell_v1_get_layer_surface(
         state->layer_shell, info->surface, info->output,
@@ -44,7 +45,8 @@ int create_layer_surface(struct wayland_state *state) {
                                          ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP);
     zwlr_layer_surface_v1_set_keyboard_interactivity(info->layer_surface, 0);
 
-    zwlr_layer_surface_v1_add_listener(info->layer_surface, &zwlr_layer_surface_v1_listener, state);
+    zwlr_layer_surface_v1_add_listener(info->layer_surface,
+                                       &zwlr_layer_surface_v1_listener, state);
     wl_surface_commit(info->surface);
   }
   return 0;
